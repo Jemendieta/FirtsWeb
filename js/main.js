ec6e11a5a87f8fd3f4efa8a120612746de6383c9
Jemendieta <mendieta.86@gmail.com>
2021-04-07 23:35:35 -0500

    agrego boton de menu y código js para su funcionamiento

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+// código js de mi sitio web que actúa sobre el menu
+const change_view = window.matchMedia("screen and (max-width:767px)");
 
+const menu_prop = document.querySelector(".menu");
+
+const icon_menu = document.querySelector("#burger_icon");
+
+change_view.addListener(validation);
+
+function validation(event) {
+  if (event.matches) {
+    icon_menu.addEventListener("click", watchButton);
+  } else {
+    icon_menu.removeEventListener("click", watchButton);
+  }
+}
+
+validation(change_view);
+
+function watchButton() {
+  if (menu_prop.classList.contains("menu_active")) {
+    menu_prop.classList.remove("menu_active");
+  } else {
+    menu_prop.classList.add("menu_active");
+  }
+}
+// menu_prop.classList.add("menu_active");
